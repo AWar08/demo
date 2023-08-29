30f02aa2de9c4760e22e9f721e8c6822a033c5f3
Aryan Warathe <113835691+AWar08@users.noreply.github.com>
2023-08-29 10:07:07 +0530

    Add files via upload

---
+++ account.py
@@ -0,0 +1,30 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+import streamlit as st
+
+cred = credentials.Certificate('/Users/aryanwarathe/Downloads/minor-streamlit-4de29d405bc8.json')
+firebase_admin.initialize_app(cred)
+def app():
+ 
+   choice = st.selectbox('Login/Signup',['Login','Sign Up'])
+   if choice =='Login':
+     
+     email=st.text_input('Email Address')
+     password = st.text_input('Password',type = 'password')
+
+     st.button('Login')
+
+   else:
+
+      email=st.text_input('Email Address')
+      password = st.text_input('Password',type ='password')
+
+      username =st.text_input('enter your unique name') 
+
+      if st.button('Create my account'):
+        user =auth.create_user(email = email, password =password, uid = username) 
+
+        st.success('Account created successfully!')
+        st.markdown('Please Login using your RVCE email id and password')
+        st.balloons()
